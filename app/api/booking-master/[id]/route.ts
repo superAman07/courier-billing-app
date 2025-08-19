edd8555510ba358d16ca2d0b55e28873c43bae1f
superAman07 <rivalaman123@gmail.com>
2025-08-20 01:24:09 +0530

    updated PUT fx of booking master

--- app/api/booking-master/[id]/route.ts
+++ app/api/booking-master/[id]/route.ts
@@ -17,12 +17,24 @@ export async function GET(req: NextRequest, { params }: { params: Promise<{ id: 
 
 export async function PUT(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
   try {
-    const id= (await params).id;
+    const id = (await params).id;
     const data = await req.json();
     const booking = await prisma.bookingMaster.update({
       where: { id },
       data: {
         ...data,
+        awbNo: String(data.awbNo),
+        pin: String(data.pin),
+        pcs: Number(data.pcs),
+        invoiceValue: Number(data.invoiceValue),
+        actualWeight: Number(data.actualWeight),
+        chargeWeight: Number(data.chargeWeight),
+        invoiceWt: Number(data.invoiceWt),
+        clientBillingValue: Number(data.clientBillingValue),
+        creditCustomerAmount: Number(data.creditCustomerAmount),
+        regularCustomerAmount: Number(data.regularCustomerAmount),
+        pendingDaysNotDelivered: Number(data.pendingDaysNotDelivered),
+        shipmentCostOtherMode: Number(data.shipmentCostOtherMode),
         bookingDate: new Date(data.bookingDate),
         statusDate: data.statusDate ? new Date(data.statusDate) : null,
       },
