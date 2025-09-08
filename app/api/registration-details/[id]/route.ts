52b38a5cd6c54f0acdf831f9f90b1fb76e99a599
superAman07 <rivalaman123@gmail.com>
2025-09-09 00:12:14 +0530

    updajh

--- app/api/registration-details/[id]/route.ts
+++ app/api/registration-details/[id]/route.ts
@@ -1,45 +1,45 @@
 import prisma from "@/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET({ params }: { params: { id: string } }) {
+export async function GET(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
     try {
-        const id = params.id;
+        const id = (await params).id;
         const items = await prisma.registrationDetails.findUnique({
             where: {
                 id
             }
         })
-        if(!items) {
+        if (!items) {
             return NextResponse.json({ error: "Registration details not found" }, { status: 404 })
         }
         return NextResponse.json(items)
-    } catch {
-        return NextResponse.json({ error: "Failed to fetch registration details" }, { status: 500 })
+    } catch (e) {
+        return NextResponse.json({ error: "Failed to fetch registration details" }, { status: 500 });
     }
 }
 
-export async function PUT(req: NextRequest, {params}: {params: Promise<{id:string}>}){
-    try{
+export async function PUT(req: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    try {
         const id = (await params).id;
         const data = await req.json();
         const items = await prisma.registrationDetails.update({
-            where: {id},
+            where: { id },
             data
         })
         return NextResponse.json(items);
-    }catch{
+    } catch {
         return NextResponse.json({ error: "Failed to update registration details" }, { status: 500 })
     }
 }
 
-export async function DELETE({params}:{ params: Promise<{id:string}>}){
-    try{
+export async function DELETE({ params }: { params: Promise<{ id: string }> }) {
+    try {
         const id = (await params).id;
         const items = await prisma.registrationDetails.delete({
-            where: {id}
-        }) 
+            where: { id }
+        })
         return NextResponse.json(items);
-    }catch{
+    } catch {
         return NextResponse.json({ error: "Failed to delete registration details" }, { status: 500 })
     }
 }
