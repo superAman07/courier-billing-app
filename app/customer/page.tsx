76665b4792e67309d43cef23ddff51e4b5d0fc2e
superAman07 <rivalaman123@gmail.com>
2025-09-09 00:58:47 +0530

    updated

--- app/customer/page.tsx
+++ app/customer/page.tsx
@@ -1,7 +1,8 @@
 import CustomerForm from "@/components/CustomerForm";
+import { Suspense } from "react";
 
 export default function (){
-    return <> 
-        <CustomerForm />
-    </>
+    return <Suspense fallback={<div className="p-6 text-gray-600">Loading…</div>}>
+      <CustomerForm />
+    </Suspense>
 }
