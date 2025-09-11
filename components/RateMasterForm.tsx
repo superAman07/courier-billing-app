487ad2f6fb3d44eff5b94c97295001736efb7c8f
superAman07 <rivalaman123@gmail.com>
2025-09-11 10:52:41 +0530

    sjdfidj

--- components/RateMasterForm.tsx
+++ components/RateMasterForm.tsx
@@ -225,7 +225,6 @@ export default function RateMasterForm() {
                                 <label htmlFor="mode" className={labelStyle}>Mode</label>
                                 <select name="mode" value={formData.mode} onChange={handleChange} className={inputStyle}>
                                     <option value="ALL">ALL</option>
-                                    <option value="">Select</option>
                                     <option value="AIR">Air</option>
                                     <option value="EXPRESS">Express</option>
                                     <option value="PREMIUM">Premium</option>
