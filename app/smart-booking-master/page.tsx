b1bb5bd3a2a146414e562c8f8f4cfd9b13c450a7
superAman07 <rivalaman123@gmail.com>
2025-09-12 10:55:16 +0530

    idpis

--- app/smart-booking-master/page.tsx
+++ app/smart-booking-master/page.tsx
@@ -673,7 +673,7 @@ export default function SmartBookingMasterPage() {
     };
 
     return (
-        <div className="max-w-7xl mx-auto p-8">
+        <div className="max-w-[1440px] mx-auto p-8">
             <div className="flex items-center justify-between mb-6">
                 <div>
                     <h1 className="text-3xl font-bold text-gray-900">Smart Booking Master</h1>
